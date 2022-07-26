92bfad9f2cddc6b69c831a69c0682654fb70bae8
Kevin Zheng <kevzheng27@gmail.com>
2022-07-26 19:13:38 -0400

    commit tree 6ee7689e6ca93e28906ef851c122ca1f52ae79ae

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from './Links'
 
-function About() {
+function bioExists(bio) {
+  if(bio) {
+    return (
+      <p>{bio}</p>
+    )
+  } 
+  else return null
+} 
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioExists(props.bio)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
